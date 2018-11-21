6108c991017acc2b694229fa410ccb4c238c912b
andyallenby <andy.allenby@googlemail.com>
2018-11-21 20:35:34 +0000

    QueryRoute

--- HttpTriggerCSharp.cs
+++ HttpTriggerCSharp.cs
@@ -7,27 +7,66 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Json;
+
 
 namespace Company.Function
 {
-    public static class HttpTriggerCSharp
+    public static class HTTPTriggerGetRoute
     {
-        [FunctionName("HttpTriggerCSharp")]
+        [FunctionName("HTTPTriggerGetRoute")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "Route")] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            
+            string appId = req.Query["appId"];
+            string appCode = req.Query["appCode"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            
+            appId = appId ?? data?.appId;
+            appCode = appCode ?? data?.appCode;
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            ProcessRepositories(appId,appCode).Wait();
+
+            //return name != null
+            //    ? (ActionResult)new OkObjectResult($"Hello, {name}")
+            //    : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            return (ActionResult)new OkObjectResult($"Hello, {appId}");
+        }
+
+        private static readonly HttpClient client = new HttpClient();
+        private static async Task ProcessRepositories(string appId, string appCode)
+        {
+            var serializer = new DataContractJsonSerializer(typeof(List<routes>));
+            client.DefaultRequestHeaders.Accept.Clear();
+            //client.DefaultRequestHeaders.Accept.Add(
+            //new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            //client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var stringTask = client.GetStringAsync($"https://route.api.here.com/routing/7.2/calculateroute.json?app_id={appId}&app_code={appCode}&waypoint0=geo!52.5,13.4&waypoint1=geo!52.5,13.45&mode=fastest;car;traffic:disabled");
+            var streamTask = client.GetStreamAsync($"https://route.api.here.com/routing/7.2/calculateroute.json?app_id={appId}&app_code={appCode}&waypoint0=geo!52.5,13.4&waypoint1=geo!52.5,13.45&mode=fastest;car;traffic:disabled");
+            var routes = serializer.ReadObject(await streamTask) as List<routes>;
+            var msg = await stringTask;
+            Console.Write(msg);
+            foreach (var route in routes)
+            Console.WriteLine(route.start);
+    
         }
     }
+
+    public class routes
+    {
+        public string start;
+    }
+
+   
 }
+
